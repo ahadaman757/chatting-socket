4585b45805dd3ffe6f9b597df6c387fc0ca2951a
ahadaman757 <108423683+ahadaman757@users.noreply.github.com>
2022-09-16 18:17:22 +0500

    Initial

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", false);
+
+  // Pass to next layer of middleware
+  next();
+});
+const http = require("http");
+const server = http.Server(app);
+app.get("/", jsonParser, (req, res) => {
+  res.send("Yo");
+  //   res.json("WOW");
+});
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
